46273b9a287e70515c449189a52bba5f84dcd76f
Anil <Anilmaity77@gmail.com>
2024-04-03 22:15:39 +0530

    added ppm.ino file

---
+++ RK_cinematics/ppm.ino
@@ -0,0 +1,81 @@
+
+//specifing  arrays and variables to store values
+
+void ppm_setup() {
+  pinMode(18, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(18), read_me, FALLING);
+  // enabling interrupt at pin 2
+}
+
+
+void read_me() {
+
+  a = micros();  //store time value a when pin value falling
+  c = a - b;     //calculating  time inbetween two peaks
+  b = a;         //
+  x[i] = c;      //storing 15 value in  array
+  i = i + 1;
+  if (i == 15) {
+    for (int j = 0; j < 15; j++) {
+      if (x[j] > 0){
+        
+        ch1[j] = x[j];
+
+      }
+    }
+    i = 0;
+  }
+}
+
+
+void read_rc() {
+  int i, j, k = 0;
+  for (k = 14; k > -1; k--) {
+    if (ch1[k] > 5500) { j = k; }
+  }  //detecting separation  space 10000us in that another array
+  for (i = 1; i <= 6; i++) { 
+    if(ch1[i] != 0)
+    {
+    ch[i] = ch1[i + j]; 
+    }
+    }
+}
+
+
+
+
+
+
+void evaluteinputs() {
+
+  if (ch[5] >= 1600) {
+    if (ch[3] <= 2100 && ch[3] >= 1520) {
+      throttle = 0.8 * throttle + 0.2 * map(ch[3], 1500, 2000, initial_throttle, max_limit);
+    } else if (ch[3] <= 1480 && ch[3] >= 900) {
+      throttle = 0.8 * throttle + 0.2 * map(ch[3], 1000, 1500, -max_limit, -initial_throttle_backward );
+    } else {
+      throttle = 0;
+    }
+
+
+    if (ch[1] <= 2100 && ch[1] >= 1510) {
+      sterring_value = map(ch[1], 1500, 2000, 548, 900); // 912
+    } else if (ch[1] <= 1490 && ch[1] >= 900) {
+      sterring_value = map(ch[1], 1000, 1500, 240, 548); // 548 // 227
+    } else {
+      sterring_value = 548;
+    }
+
+
+    if (ch[2] <= 1510 && ch[2] >= 900) {
+      Brake = map(ch[2], 1500, 1000, 90, 42);
+    } 
+
+
+
+  } else {
+
+    throttle = 0;
+  }
+}
+
