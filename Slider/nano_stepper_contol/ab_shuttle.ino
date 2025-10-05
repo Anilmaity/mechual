16d0131b8d625caf788ba994c73cc1fd160e7635
Anil <anilmaity77@gmail.com>
2025-10-05 10:36:33 +0530

    bug fix AB shuttle

--- Slider/nano_stepper_contol/ab_shuttle.ino
+++ Slider/nano_stepper_contol/ab_shuttle.ino
@@ -2,19 +2,12 @@ long int start_position = 0;
 
 void handleABShuttle(int A, int B, int speedAtoB, int speedBtoA, int loops) {
     static int loopsDone = 0;           // Tracks completed A-to-B-to-A cycles
-    static bool goingToB = false;       // False: move to A; True: move to B
+    static bool goingToB = start_position < rightLimitPos + B ? true : false;       // False: move to A; True: move to B
     static bool firstRun = true;        // Ensures first move is to A
   
     int compare_loop = loops;
     // Ensure A < B for consistency
 
-    // if (B > A) {
-    //   if(rightLimitPos + start_position < rightLimitPos + A)
-    //   {
-    //     int compare_loop = loops -1;
-    //   }
-
-    // }
 
     // else if (A > B)
     // {
@@ -51,6 +44,7 @@ void handleABShuttle(int A, int B, int speedAtoB, int speedBtoA, int loops) {
                     firstRun = false;
                     goingToB = true;
                 }
+
         } else {
             // Switch direction
             goingToB = !goingToB;
